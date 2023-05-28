1757ef3fa9431469209b643a5a4ffc66783e4e3e
Martin Petrovski <mpetrovski16@gmail.com>
2023-05-28 14:30:09 +0200

    init

---
+++ VizuelnoProektna/Blocks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizuelnoProektna
+{
+    public class Blocks
+    {
+        public static int Width = 40;
+        public static int Height = 20;
+        public Point Position { get; set; }
+        public Color Color { get; set; }
+        public Random Random { get; set; } = new Random();
+        public int State { get; set; }
+        public Blocks() { 
+            State = Random.Next(3);
+            switch (State)
+            {
+                case 0:
+                    Color = Color.Red;
+                    break;
+                case 1:
+                    Color = Color.Green;
+                    break;
+                case 2:
+                    Color = Color.Blue;
+                    break;
+            }
+        }
+    }
+}
---
+++ VizuelnoProektna/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizuelnoProektna
+{
+    public abstract class Player
+    {
+        public int Width { set; get; }
+        public int Height { set; get; }
+        public Point Position { get; set; }
+        public Color Color { get; set; }
+
+        protected Player(int width, int height, Point position, Color color)
+        {
+            Width = width;
+            Height = height;
+            Position = position;
+            Color = color;
+        }
+
+        public abstract void Draw(Graphics g);
+        public abstract void Move(int dx, int dy);
+    }
+}
